17263c3c772ea8c2d89780151fbf6359bc6c7d2d
martin <martin@uschan.biologie.hu-berlin.de>
2011-08-19 11:30:54 +0200

    Extracting intensities

--- src/batch_analyze_dots_distribution.py
+++ src/batch_analyze_dots_distribution.py
@@ -5,28 +5,47 @@ from os.path import join, split, exists
 SIC_ROOT = '/home/basar/Personal/Martin_Seeger/working_directory' 
 SIC_PROCESSED = "processed"
 
-path=join(SIC_ROOT, SIC_PROCESSED)
-outfile = join(path, "all_spots.xls")
 
-if exists(outfile): os.remove(outfile)
+def aggregate_spots(path=join(SIC_ROOT, SIC_PROCESSED)):
+    '''Aggregate all spots in current directory into matrix and write into one .csv file'''
+    outfile = join(path, "all_spots.xls")
+    if exists(outfile): os.remove(outfile)
+    
+    with open(outfile, "a") as outfile:
+        # Write file header
+        outfile.write("\t".join(["FileID", "CellID", "x", "y", "pixels", "f.tot", "f.median", "f.mad"]))
+        outfile.write("\n")
+    
+        l = listdir(path)
+        spots = []
+        for filename in l:
+            if filename.find("SPOTS") != -1:
+                print "Spotty file found:", filename
+                f = open(join(path, filename), 'r')
+                ls = f.readlines()
+                for line in ls[1:]: # we start at 1 because we do not need another header
+                    splitline = line.split(" ")
+                    splitline.insert(0, splitline[-1].strip()) # fetches last item (here: file ID) and prepends
+                    #print "\t".join(splitline[:-1])
+                    # for the matrix, strings are converted into ints and floats
+                    spot = [splitline[0], splitline[1], float(splitline[2]), float(splitline[3]), int(splitline[4]), int(splitline[5]), float(splitline[6]), float(splitline[7])]
+                    spots.append(spot)
+                    outfile.write("\t".join(splitline[:-1]))
+                    outfile.write("\n")
+    outfile.close()
+    print "Finished."
+    return spots
 
-with open(outfile, "a") as outfile:
-    # Write file header
-    outfile.write("\t".join(["FileID", "CellID", "x", "y", "pixels", "f.tot", "f.median", "f.mad"]))
-    outfile.write("\n")
 
-    l = listdir(path)
-    for filename in l:
-        if filename.find("SPOTS") != -1:
-            print "Spotty file found:", filename
-            f = open(join(path, filename), 'r')
-            ls = f.readlines()
-            for line in ls[1:]:
-                splitline = line.split(" ")
-                splitline.insert(0, splitline[-1].strip()) # fetches last item (here: file ID) and prepends
-                #print "\t".join(splitline[:-1])
-                outfile.write("\t".join(splitline[:-1]))
-                outfile.write("\n")
-outfile.close()
-        
-print "Finished."
+def analyze_intensities(spots):
+    print "Analyzing spot intensities..."
+    def column(matrix, i):
+        return [row[i] for row in matrix]
+
+    intensities = column(spots, 5)
+    print sorted(intensities)
+    print "Finished analyzing spot intensities."
+
+if __name__ == '__main__':
+    spots = aggregate_spots()
+    analyze_intensities(spots)
