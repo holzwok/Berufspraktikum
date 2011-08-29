fc8845fc4f45283682104a7bda9bf9aac08f5b9d
martin <martin@uschan.biologie.hu-berlin.de>
2011-08-29 16:48:39 +0200

    Minor

---
+++ src/set_cell_id_parameters.py
@@ -0,0 +1,39 @@
+
+#MACHINE = "sstoma-pokrzywa"
+#MACHINE = "sstoma-smeik"
+MACHINE = "martin-uschan"
+#MACHINE = "MJS Windows"
+#MACHINE = "MJS Linux"
+
+if MACHINE == "sstoma-smeik":
+    SIC_CELLID = "/home/sstoma/svn/sstoma/src/11_01_25_cellId/cell"
+    SIC_ROOT = '/local/home/sstoma/images/11-06-18-sic,matthias'
+    SIC_FIJI = '/home/sstoma/bin/Fiji.app/fiji-linux64'
+    SIC_SPOTTY = ''
+elif MACHINE == "sstoma-pokrzywa":
+    SIC_CELLID = "/Users/stymek/src/cell_id-1.4.3-HACK/cell"
+    SIC_ROOT = '/Volumes/image-data/images/11-01-10-mateo,aouefa,dataanalysis-test'
+    SIC_FIJI = 'fiji-macosx'
+    SIC_SPOTTY = ''
+elif MACHINE == "martin-uschan":
+    SIC_CELLID = "/home/basar/Personal/Martin_Seeger/imaging/cell_id-143_hack/cell"
+    SIC_ROOT = '/home/basar/Personal/Martin_Seeger/working_directory' 
+    SIC_FIJI = '/home/basar/Personal/Martin_Seeger/imaging/Fiji.app/fiji-linux64'
+    SIC_SPOTTY = '/home/basar/Personal/Martin_Seeger/workspace/Berufspraktikum/src/spottyG.R'
+elif MACHINE == "MJS Windows":
+    SIC_CELLID = r'C:/Program Files (x86)/VCell-ID/bin/vcellid.exe' #TODO: working? or Progra~2 hack?
+    SIC_ROOT = r'C:/Users/MJS/My Dropbox/Studium/Berufspraktikum/working_directory'
+    SIC_FIJI = r'C:/Program Files/Fiji.app/fiji-win64.exe'
+    SIC_SPOTTY = ''
+elif MACHINE == "MJS Linux":
+    SIC_CELLID = "/home/mjs/Berufspraktikum/imaging/cell_id-1.4.3_hack/cell"
+    SIC_ROOT = '/home/mjs/Berufspraktikum/working_directory' 
+    SIC_FIJI = '/usr/bin/fiji' #'/home/mjs/Berufspraktikum/Fiji.app/fiji-linux64' # <- this one does not work
+    SIC_SPOTTY = ''
+
+
+def set_parameters():
+    print "hello"    
+
+if __name__ == '__main__':
+    set_parameters()
