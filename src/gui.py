cd3786459d2da2801a96ea02b1e5d7b43cd4c4b8
martin <martin@uschan.biologie.hu-berlin.de>
2011-09-09 13:49:24 +0200

    Session in GUI

--- src/gui.py
+++ src/gui.py
@@ -6,7 +6,7 @@ from datetime import date
 from PyQt4 import QtCore, QtGui
 from MainWindow import Ui_notepad
 
-from global_vars import PARAM_DICT
+#from global_vars import PARAM_DICT, SIC_PROCESSED, SIC_RESULTS, SIC_SCRIPTS, SIC_LINKS
 from set_cell_id_parameters import set_parameters
 from main import * #prepare_structure
 
@@ -18,13 +18,21 @@ class StartQT4(QtGui.QMainWindow):
         self.ui.setupUi(self)
         QtCore.QObject.connect(self.ui.button_change_cellid_parameters, QtCore.SIGNAL("clicked()"), self.change_cell_id_dialog)
         QtCore.QObject.connect(self.ui.button_load_default_cellid_parameters, QtCore.SIGNAL("clicked()"), self.load_cell_id_dialog)
-        QtCore.QObject.connect(self.ui.prepare_structure, QtCore.SIGNAL("clicked()"), self.prepare_structure)
+
         QtCore.QObject.connect(self.ui.working_directory, QtCore.SIGNAL("clicked()"), self.working_directory_dialog)
         QtCore.QObject.connect(self.ui.cell_id_executable, QtCore.SIGNAL("clicked()"), self.cell_id_executable_dialog)
         QtCore.QObject.connect(self.ui.fiji_executable, QtCore.SIGNAL("clicked()"), self.fiji_executable_dialog)
         QtCore.QObject.connect(self.ui.spottyR_file, QtCore.SIGNAL("clicked()"), self.spottyR_file_dialog)
         QtCore.QObject.connect(self.ui.pb_save_preferences, QtCore.SIGNAL("clicked()"), self.save_preferences_dialog)
         QtCore.QObject.connect(self.ui.pb_load_preferences, QtCore.SIGNAL("clicked()"), self.load_preferences_dialog)
+
+        QtCore.QObject.connect(self.ui.pb_images_directory, QtCore.SIGNAL("clicked()"), self.images_directory_dialog)
+        QtCore.QObject.connect(self.ui.pb_save_session, QtCore.SIGNAL("clicked()"), self.save_session_dialog)
+        QtCore.QObject.connect(self.ui.pb_apply_session, QtCore.SIGNAL("clicked()"), self.apply_session_dialog)
+        QtCore.QObject.connect(self.ui.pb_load_session, QtCore.SIGNAL("clicked()"), self.load_session_dialog)
+
+        QtCore.QObject.connect(self.ui.prepare_structure, QtCore.SIGNAL("clicked()"), self.prepare_structure)
+
         QtCore.QObject.connect(self.ui.button_save, QtCore.SIGNAL("clicked()"), self.file_save)
     
     def change_cell_id_dialog(self):
@@ -91,7 +99,7 @@ class StartQT4(QtGui.QMainWindow):
         preferences_dict["fijiexe"] = SIC_FIJI
         preferences_dict["spottyfile"] = SIC_SPOTTY
         defaultFileName = "Preferences_"+str(date.today())+".pref"
-        filename = str(QtGui.QFileDialog.getSaveFileName(None, QtCore.QString("Save preferences"), defaultFileName));
+        filename = str(QtGui.QFileDialog.getSaveFileName(None, QtCore.QString("Save preferences..."), defaultFileName));
         preferences_file = open(filename, 'w')
         pickle.dump(preferences_dict, preferences_file)
 
@@ -112,17 +120,50 @@ class StartQT4(QtGui.QMainWindow):
         self.ui.lineEditfiji_executable.setText(SIC_FIJI)
         self.ui.lineEditspottyR_file.setText(SIC_SPOTTY)
          
+    def images_directory_dialog(self):
+        imagesdir = QtGui.QFileDialog.getExistingDirectory(self, "Select", ".", options = QtGui.QFileDialog.DontResolveSymlinks)
+        if imagesdir:
+            self.ui.le_images_directory.setText(imagesdir)
+        global SIC_ORIG 
+        SIC_ORIG = str(imagesdir) 
 
+    def save_session_dialog(self):
+        global NIBA_ID 
+        NIBA_ID = str(self.ui.le_niba_id.text())
+        global DIC_ID 
+        DIC_ID = str(self.ui.le_dic_id.text())
+        session_dict = {}
+        session_dict["imagesdir"] = SIC_ORIG
+        session_dict["niba_id"] = NIBA_ID
+        session_dict["dic_id"] = DIC_ID
+        defaultFileName = "Session_"+str(date.today())+".ssn"
+        filename = str(QtGui.QFileDialog.getSaveFileName(None, QtCore.QString("Save session..."), defaultFileName));
+        session_file = open(filename, 'w')
+        pickle.dump(session_dict, session_file)
+
+    def apply_session_dialog(self):
+        global SIC_ORIG 
+        SIC_ORIG = str(self.ui.le_images_directory.text()) 
+        global NIBA_ID 
+        NIBA_ID = str(self.ui.le_niba_id.text())
+        global DIC_ID 
+        DIC_ID = str(self.ui.le_dic_id.text())
+    
+    def load_session_dialog(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self, "Select", ".")
+        session_file = open(filename, 'r')
+        session_dict = pickle.load(session_file)
+        global SIC_ORIG 
+        SIC_ORIG = session_dict["imagesdir"]
+        global NIBA_ID 
+        NIBA_ID = session_dict["niba_id"] 
+        global DIC_ID 
+        DIC_ID = session_dict["dic_id"]
+        self.ui.le_images_directory.setText(SIC_ORIG)
+        self.ui.le_niba_id.setText(NIBA_ID)
+        self.ui.le_dic_id.setText(DIC_ID)
+    
     def prepare_structure(self):
-        # Das wird später von der GUI gesetzt:
-        SIC_ORIG = "orig3" # folder with original images, they are not edited
-        NIBA_ID = "w1NIBA"
-        DIC_ID = "w3DIC"
-        SIC_PROCESSED = "processed" # folder with processed images, images may be changed, symlinks are used to go down with the size 
-        SIC_RESULTS = "results"
-        SIC_SCRIPTS = "scripts"
-        SIC_LINKS = "processed"
-
         prepare_structure(path=SIC_ROOT,
                       skip=[SIC_ORIG, SIC_SCRIPTS, "orig", "orig1", "orig2", "orig3", "orig4", "orig5", "orig6"],
                       create_dirs=[SIC_PROCESSED, SIC_RESULTS, SIC_LINKS],
