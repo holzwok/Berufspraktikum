78ab1a441d41820d124c7ef8371fc6878c731159
MJS <mjseeger@gmail.com>
2012-01-26 22:14:50 +0100

    SpotTracking 1.0

--- SpotTracking/ST2MTJ.py
+++ SpotTracking/ST2MTJ.py
@@ -1,29 +1,39 @@
 import fileinput
+import os
+from os.path import join
+from dircache import listdir
 
-infilename = "Spot Tracker Results.xls"
-outfilename = "Spot Tracker Results.mdf"
+#path = "C:\Users\MJS\Workspace_2\Berufspraktikum\SpotTracking" # change this as desired
+path = os.getcwd()
 
-infile = open(infilename, 'r')
+outfilename = "Spot_Tracker_Results.mdf"
+trackID = 1
 
 writestring = "MTrackJ 1.5.0 Data File\n"
 writestring += "Displaying true true true 1 2 0 4 100 10 0 0 0 2 1 12 0 false false false false\n"
 writestring += "Assembly 1 FF0000\n"
 writestring += "Cluster 1 FFFF00\n"
-writestring += "Track 1 FF0000 true\n"
 
-for i, line in enumerate(fileinput.input([infilename])):
-    if not fileinput.isfirstline():
-        writestring += "Point " + str(i) + " "
-        linelist = line.split()
-        writestring += linelist[1]+" " # x
-        writestring += linelist[2]+" " # y
-        writestring += linelist[0]+" " # t
-        writestring += "1.0\n" # z
+l = listdir(path)
+for infilename in l:
+    if infilename.endswith(".xls"):
+        writestring += "Track %i FF0000 true\n" % trackID
+        
+        for i, line in enumerate(fileinput.input([join(path, infilename)])):
+            if not fileinput.isfirstline():
+                writestring += "Point " + str(i) + " "
+                linelist = line.split()
+                writestring += linelist[1]+" " # x
+                writestring += linelist[2]+" " # y
+                writestring += "1.0 %.1f " % float(linelist[0]) # z, t
+                writestring += "1.0\n" # c
+
+        trackID += 1
 
 writestring += "End of MTrackJ Data File\n"
 
-print writestring
-
-outfile = open(outfilename, "w")
+outfile = open(join(path, outfilename), "w")
 outfile.write(writestring)
 outfile.close()
+
+print "Done."
