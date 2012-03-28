da5eb98c4a2988e888957db4235ffceffcff5453
martin <martin@abacus.biologie.hu-berlin.de>
2012-03-28 17:44:09 +0200

    Cell profiler post-processing (work in progress)

---
+++ CP/cp_main.py
@@ -0,0 +1,12 @@
+from dircache import listdir
+
+path = "/home/basar/Personal/Martin_Seeger/CellProfiler_work/output"
+
+l = listdir(path)
+for infilename in l:
+    if infilename.startswith("MAX_"):
+        print infilename
+        # maske anwenden:
+        # mit .loc file mergen
+        # nur punkte filtern/einzeichnen, fuer die maske == true
+        
