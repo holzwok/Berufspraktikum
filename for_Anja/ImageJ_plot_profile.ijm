9691de85f9db1cf9db8c6fafa5a4b724b16a118e
martin <martin@abacus.biologie.hu-berlin.de>
2012-04-13 14:47:42 +0200

    First steps in FIJI script for Anja

---
+++ for_Anja/ImageJ_plot_profile.ijm
@@ -0,0 +1,5 @@
+
+makeLine(222, 100, 315, 412);
+run("Plot Profile");
+close();
+
---
+++ for_Anja/ij_in_fiji_playground.py
@@ -0,0 +1,53 @@
+from ij import IJ
+from ij.io import FileSaver
+from ij.process import ImageStatistics as IS
+import os
+from os import path
+
+imp = IJ.getImage()
+fs = FileSaver(imp)
+
+# Print image details
+print "title:", imp.title
+print "width:", imp.width
+print "height:", imp.height
+print "number of pixels:", imp.width * imp.height
+print "number of slices:", imp.getNSlices()
+print "number of channels:", imp.getNChannels()
+print "number of time frames:", imp.getNFrames()
+
+types = {ImagePlus.COLOR_RGB : "RGB",
+         ImagePlus.GRAY8 : "8-bit",
+         ImagePlus.GRAY16 : "16-bit",
+         ImagePlus.GRAY32 : "32-bit",
+         ImagePlus.COLOR_256 : "8-bit color"}
+
+print "image type:", types[imp.type]
+
+# Get its ImageProcessor
+ip = imp.getProcessor()
+
+options = IS.MEAN | IS.MEDIAN | IS.MIN_MAX
+stats = IS.getStatistics(ip, options, imp.getCalibration())
+
+# print statistics on the image
+print "Image statistics for", imp.title
+print "Mean:", stats.mean
+print "Median:", stats.median
+print "Min and max:", stats.min, "-", stats.max
+          
+
+# A known folder to store the image at:
+folder = "/home/basar/Personal/Martin_Seeger/workspace/Berufspraktikum/for_Anja"
+
+# Test if the folder exists before attempting to save the image:
+if path.exists(folder) and path.isdir(folder):
+    print "folder exists:", folder
+    filepath = folder + "/" + "testpic.tif"
+    if path.exists(filepath):
+        print "File exists! Not saving the image, would overwrite a file!"
+    elif fs.saveAsTiff(filepath):
+        print "File saved successfully at ", filepath
+else:
+	print "Folder does not exist or it's not a folder!"
+          
