b582159836dcd5a54778d0082b86391659dbd0c8
martin <martin@abacus.biologie.hu-berlin.de>
2012-04-13 20:16:52 +0200

    End of day/work in progress

--- for_Anja/tube_measurer.py
+++ for_Anja/tube_measurer.py
@@ -1,6 +1,6 @@
 from ij import IJ
 from ij.process import ImageStatistics as IS
-from ij.gui import Roi, Line, PolygonRoi  
+from ij.gui import Roi, Line  
 from ij.process import FloatProcessor
 
 imp = IJ.getImage()
@@ -42,20 +42,9 @@ for bp in brightestpixellist:
     y = bp/imp.width
     print x, y
 
-# Fill a polygonal region of interest
-# with a value of -3
-xs = [234, 174, 162, 102, 120, 123, 153, 177, 171,
-      60, 0, 18, 63, 132, 84, 129, 69, 174, 150,
-      183, 207, 198, 303, 231, 258, 234, 276, 327,
-      378, 312, 228, 225, 246, 282, 261, 252]
-ys = [48, 0, 60, 18, 78, 156, 201, 213, 270, 279,
-      336, 405, 345, 348, 483, 615, 654, 639, 495,
-      444, 480, 648, 651, 609, 456, 327, 330, 432,
-      408, 273, 273, 204, 189, 126, 57, 6]
-proi = PolygonRoi(xs, ys, len(xs), Roi.POLYGON)
 fp = FloatProcessor(width, height, pixels, None)  
-fp.setRoi(proi)
-fp.setValue(2)
-fp.fill(proi.getMask())  # Attention!
-
-imp.show()
+roi = Line(0, 0, 100, 100)
+fp.setRoi(roi)
+fp.setValue(8000.0)
+fp.fill()
+ImagePlus("bla", fp).show()
