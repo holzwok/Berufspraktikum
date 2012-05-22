02f71e6fb5b756a69cd215edea335efd6b75e975
MJS <MJS@192.168.1.15>
2012-05-22 13:50:20 +0200

    Minor

--- for_Anja/tube_measurer.py
+++ for_Anja/tube_measurer.py
@@ -4,33 +4,32 @@ from ij.gui import Roi, Line
 from ij.process import FloatProcessor
 from math import cos, sin, pi
 
-
 def all_indices(value, qlist):
 	# returns list of indices of elements in qlist that have value
-    indices = []
-    idx = -1
-    while True:
-        try:
-            idx = qlist.index(value, idx+1)
-            indices.append(idx)
-        except ValueError:
-            break
-    return indices
+	indices = []
+	idx = -1
+	while True:
+		try:
+			idx = qlist.index(value, idx+1)
+			indices.append(idx)
+		except ValueError:
+			break
+		return indices
 
 def moving_average(data, win):
-    if not win%2: # even
-        return [sum(data[x-win/2:x+win/2])/win for x in range(win/2, len(data)-win/2+1)]
-    else:         # odd
-        return [sum(data[x-(win-1)/2:x+(win+1)/2]/win) for x in range((win-1)/2, len(data)-(win+1)/2+1)]
+	if not win%2: # even
+		return [sum(data[x-win/2:x+win/2])/win for x in range(win/2, len(data)-win/2+1)]
+	else:         # odd
+		return [sum(data[x-(win-1)/2:x+(win+1)/2]/win) for x in range((win-1)/2, len(data)-(win+1)/2+1)]
 
 def median(numericValues):
 	theValues = sorted(numericValues)
-	if len(theValues) % 2 == 1:
+	if len(theValues)%2 == 1:
 		return theValues[(len(theValues)+1)/2-1]
 	else:
 		lower = theValues[len(theValues)/2-1]
 		upper = theValues[len(theValues)/2]
-	return (float(lower + upper)) / 2  
+	return (float(lower + upper))/2  
 
 
 imp = IJ.getImage()
@@ -53,17 +52,17 @@ brightestpixellist = all_indices(stats.max, list(pixels))
 #print "pixels with max. value:", brightestpixellist
 
 for bp in brightestpixellist:
-    x = bp%imp.width
-    y = bp/imp.width
-    print "max. brightness coordinates:", x, y
+	x = bp%imp.width
+	y = bp/imp.width
+	#print "max. brightness coordinates:", x, y
 
-#ip.setValue(8000.0)
-
-# note that the following chooses for x, y the last element in the brightestpixelist
-# TODO: nicer would be to loop over this list or somehow specify which element to pick
+# note that the following chooses for x, y the last element in the brightestpixelist which is OK if there is only one element
+# TODO: would be nicer to loop over this list or specify which element to pick
 
 length = 400/2
-for alpha in range(0, 180, 5):
+angular_accuracy = 5 # degrees
+
+for alpha in range(0, 180, angular_accuracy):
 	myimp = ImagePlus("Profile_"+str(alpha), ip)
 	vecx, vecy = length * cos(alpha * pi/180), length * sin(alpha * pi/180)
 	roi = Line(x-vecx, y-vecy, x+vecx, y+vecy)
@@ -79,7 +78,7 @@ for alpha in range(0, 180, 5):
 	profarray = profplot.getProfile()
 
 	# smooth the data by calculating moving average
-	win = 12 # must be even
+	win = 12 # window size for moving average, must be even
 	movavg = moving_average(profarray, win)
 	medi = median(movavg)
 	#print "median of profile:", medi
@@ -94,10 +93,10 @@ for alpha in range(0, 180, 5):
 	#print "maxpositions =", maxpositions
 	if len(maxpositions)==2:
 		tubewidth = min(tubewidth, abs(maxpositions[1] - maxpositions[0]))
-		print "width =", tubewidth, "pixels."
 	
 	# create plot
 	#plot = Plot("alpha = "+str(alpha), "xlabel", "ylabel", range(win/2-1, len(movavg)-win/2), movavg)
 	#plot.show()
 
+print "width =", tubewidth, "pixels."
 #ImagePlus("Profile", ip).show()
