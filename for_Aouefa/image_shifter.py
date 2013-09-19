91897a5e90d6332e2609b2cfb56ee19b65a51d5d
gittenberg <mjseeger@gmail.com>
2013-09-19 14:33:22 +0200

    Minor

--- for_Aouefa/image_shifter.py
+++ for_Aouefa/image_shifter.py
@@ -1,4 +1,28 @@
+import Image
+import os
+from dircache import listdir
 
-x = 14
+dirname = "/home/basar/shared/20130808_Alignment_BF_fluo"
 
-print "hello"
+x = -3
+y = 14
+
+l = listdir(dirname)
+
+'''
+for filename in l:
+    if filename.endswith(".TIF") or filename.endswith(".tif"):
+        print filename
+        fullname = os.path.join(dirname, filename)
+
+        im = Image.open(fullname)
+        #im.show()
+        
+        filegroup_id = "_".join(filename.split("_")[:-1])
+        filetype_id = filename.split("_")[-1]
+        
+        print filegroup_id
+        print filetype_id
+'''
+
+print l[0]
